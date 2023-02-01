b8e9f68720d9872cd59791592f3f27be4a233b30
asb1512 <asb1512@gmail.com>
2023-02-01 12:44:33 -0500

    added mobile navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,16 +2,23 @@ import styles from './Navbar.module.css';
 
 export default function Navbar() {
   return (
-    <nav className={styles.wrapper}>
-      <div className={styles.left}>
-        <a>HOME</a>
-        <a>MISSION</a>
-        <a>BOARD</a>
-      </div>
+    <>
+      <nav className={`${styles.wrapper} ${styles.general}`}>
+        <div className={styles.left}>
+          <a>HOME</a>
+          <a>MISSION</a>
+          <a>BOARD</a>
+        </div>
 
-      <div>
-        <a className={styles.right}>DONATE</a>
-      </div>
-    </nav>
+        <div className={styles.right}>
+          <a className={styles.link}>DONATE</a>
+        </div>
+      </nav>
+
+      {/* mobile navbar */}
+      <nav className={styles.mobileWrapper}>
+      
+      </nav>
+    </>
   );
 }
