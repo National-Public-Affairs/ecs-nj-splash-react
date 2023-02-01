46a577457aca3000cb20276ed68b332e429a871c
asb1512 <asb1512@gmail.com>
2023-02-01 16:44:31 -0500

    building out footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import logo from '../../assets/logo.png';
+import styles from './Footer.module.css';
+
+export default function Footer() {
+  return (
+    <div className={styles.wrapper}>
+      <img
+        src={logo}
+        alt="Elect Common Sense – NJ"
+        className={styles.logo}
+      />
+    </div>
+  );
+}
