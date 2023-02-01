9c33cb086913f565dce0fd9f9f980181dd4e531f
asb1512 <asb1512@gmail.com>
2023-02-01 16:43:51 -0500

    created a root route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Home from './pages/Home/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
