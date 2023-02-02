9a7bf0c821f15cc66623de2694ef06c8de134c04
asb1512 <asb1512@gmail.com>
2023-02-02 12:30:48 -0500

    added splat route

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/mission" element={<Mission />} />
+        <Route path="*" element={null} />
       </Routes>
       <Footer />
     </div>
