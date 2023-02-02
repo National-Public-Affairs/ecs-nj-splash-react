8e88e5884193e0ddccefa26ec9e3a86596a02d47
asb1512 <asb1512@gmail.com>
2023-02-02 11:28:05 -0500

    removed logo from footer; changed msg

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,16 +1,9 @@
-import logo from '../../assets/logo.png';
 import styles from './Footer.module.css';
 
 export default function Footer() {
   return (
     <div className={styles.wrapper}>
-      <img
-        src={logo}
-        alt="Elect Common Sense – NJ"
-        className={styles.logo}
-      />
-
-      <div>®All Rights Reserved.</div>
+      <div>Paid for by Elect Common Sense</div>
     </div>
   );
 }
