d80305d98959974dd0a9ed10f247cab9edf54f4b
asb1512 <asb1512@gmail.com>
2023-02-06 11:03:35 -0500

    setting border with props

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,14 +1,28 @@
+import { ReactNode } from 'react';
 import styles from './Tile.module.css';
 
 type Props = {
-  icon: string,
+  icon: ReactNode | string,
   text: string,
+  borderColor: '#ef0b2e' | '#3a3393',
 }
 
-export default function Tile({ icon, text }: Props) {
+export default function Tile({ icon, text, borderColor }: Props) {
   return (
-    <div className={styles.wrapper}>
-      <img src={icon} alt="Elect Common Sense" />
+    <div
+      className={styles.wrapper}
+      style={{ border: `5px solid ${borderColor}` }}
+    >
+      {
+        typeof icon === 'string'
+          ? (
+            <img
+              src={icon}
+              alt="Elect Common Sense"
+            />
+          )
+          : icon
+      }
       <div>{text}</div>
     </div>
   );
