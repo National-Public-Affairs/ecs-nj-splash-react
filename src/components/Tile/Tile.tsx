c0d780851179081840ca8ea1613ae6aa1ea503e1
asb1512 <asb1512@gmail.com>
2023-02-06 14:46:30 -0500

    unused styling

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -4,15 +4,11 @@ import styles from './Tile.module.css';
 type Props = {
   icon: ReactNode | string,
   text: string,
-  borderColor: '#ef0b2e' | '#3a3393',
 }
 
-export default function Tile({ icon, text, borderColor }: Props) {
+export default function Tile({ icon, text }: Props) {
   return (
-    <div
-      className={styles.wrapper}
-      style={{ border: `5px solid ${borderColor}` }}
-    >
+    <div className={styles.wrapper}>
       {
         typeof icon === 'string'
           ? (
