2b801608777bae8d14ee6f32f0620d7408441986
asb1512 <asb1512@gmail.com>
2023-03-13 19:26:00 -0400

    advisor cards animate in

--- src/pages/Advisors/Advisors.tsx
+++ src/pages/Advisors/Advisors.tsx
@@ -1,3 +1,4 @@
+import { useTrail, animated } from '@react-spring/web';
 import Header from '../../components/Header/Header';
 import AdvisorCard from '../../components/AdvisorCard/AdvisorCard';
 import Crimi from '../../assets/John-Crimi.jpg';
@@ -9,7 +10,64 @@ import Nardolillo from '../../assets/Greg-Nardolillo.png';
 import Gilmore from '../../assets/George-Gilmore.jpg';
 import styles from './Advisors.module.css';
 
+const pageData = [
+  <AdvisorCard
+    img={Nader}
+    name="Elizabeth Nader"
+    title="Chair"
+    subtitle=""
+  />,
+  <AdvisorCard
+    img={Gill}
+    name="Rob Gill"
+    title="CEO & Founder"
+    subtitle="EPIC Financial Strategies"
+  />,
+  <AdvisorCard
+    img={Mabey}
+    name="Sean Mabey"
+    title="President"
+    subtitle="M&B Trucking"
+  />,
+  <AdvisorCard
+    img={Crimi}
+    name="John Crimi"
+    title="Owner"
+    subtitle="County Concrete Corporation"
+  />,
+  <AdvisorCard
+    img={Nardolillo}
+    name="Greg Nardolillo"
+    title="Owner & Founder"
+    subtitle="WealthPlan Advantage"
+  />,
+  <AdvisorCard
+    img={Dimemmo}
+    name="Guy DiMemmo"
+    title="Founder & CEO"
+    subtitle="Visual Computer Solutions Inc."
+  />,
+  <AdvisorCard
+    img={Gilmore}
+    name="George Gilmore"
+    title="Senior Advisor, Elect Common Sense"
+    subtitle="Chairman, Ocean County GOP"
+  />,
+];
+
 export default function Advisors() {
+  const trail = useTrail(7, {
+    config: { mass: 1, tension: 150, friction: 100, duration: 250 },
+    from: {
+      opacity: 0,
+      transform: 'scale(0.5)',
+    },
+    to: {
+      opacity: 1,
+      transform: 'scale(1)',
+    },
+  });
+
   return (
     <div className="page">
       <Header
@@ -19,48 +77,16 @@ export default function Advisors() {
       />
 
       <div className={styles.wrapper}>
-        <AdvisorCard
-          img={Nader}
-          name="Elizabeth Nader"
-          title="Chair"
-          subtitle=""
-        />
-        <AdvisorCard
-          img={Gill}
-          name="Rob Gill"
-          title="CEO & Founder"
-          subtitle="EPIC Financial Strategies"
-        />
-        <AdvisorCard
-          img={Mabey}
-          name="Sean Mabey"
-          title="President"
-          subtitle="M&B Trucking"
-        />
-        <AdvisorCard
-          img={Crimi}
-          name="John Crimi"
-          title="Owner"
-          subtitle="County Concrete Corporation"
-        />
-        <AdvisorCard
-          img={Nardolillo}
-          name="Greg Nardolillo"
-          title="Owner & Founder"
-          subtitle="WealthPlan Advantage"
-        />
-        <AdvisorCard
-          img={Dimemmo}
-          name="Guy DiMemmo"
-          title="Founder & CEO"
-          subtitle="Visual Computer Solutions Inc."
-        />
-        <AdvisorCard
-          img={Gilmore}
-          name="George Gilmore"
-          title="Senior Advisor, Elect Common Sense"
-          subtitle="Chairman, Ocean County GOP"
-        />
+        {
+          trail.map((props, idx) => (
+            <animated.div
+              className={styles.item}
+              style={{ ...props }}
+            >
+              {pageData[idx]}
+            </animated.div>
+          ))
+        }
       </div>
     </div>
   );
