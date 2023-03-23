00c0aeb474d76b88de079bb53e7014dc0befb632
asb1512 <asb1512@gmail.com>
2023-03-23 17:32:23 -0400

    built out contact form

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,46 +1,135 @@
-import { SyntheticEvent } from 'react';
+import { useState, SyntheticEvent } from 'react';
+import { toast } from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+import Header from '../../components/Header/Header';
 import styles from './Contact.module.css';
 
+type InputType = 'firstName' | 'lastName' | 'email' | 'zip' | 'mobile' | 'textarea' | 'optIn';
+
 export default function Contact() {
+  const navigate = useNavigate();
+
+  const [firstName, setFirstName] = useState<string>('');
+  const [lastName, setLastName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [zip, setZip] = useState<string>('');
+  const [mobile, setMobile] = useState<string>('');
+  const [optIn, setOptIn] = useState<boolean>(false);
+  const [textarea, setTextarea] = useState<string>('');
+
+  const handleInputChange = (event:SyntheticEvent, type: InputType) => {  
+    switch (type) {
+      case 'firstName':
+        setFirstName((event.target as HTMLInputElement).value);
+        break;
+      case 'lastName':
+        setLastName((event.target as HTMLInputElement).value);
+        break;
+      case 'email':
+        setEmail((event.target as HTMLInputElement).value);
+        break;
+      case 'zip':
+        setZip((event.target as HTMLInputElement).value);
+        break;
+      case 'mobile':
+        setMobile((event.target as HTMLInputElement).value);
+        break;
+      case 'textarea':
+        setTextarea((event.target as HTMLInputElement).value);
+        break;
+      case 'optIn':
+        break;
+      default:
+        break;
+    }
+  };
+
   const handleSubmit = (event: SyntheticEvent<HTMLFormElement>): any => {
     event.preventDefault();
+    const toastId = toast.loading('Submitting...');
+    if (optIn) {
+      setTimeout(() => {
+        toast.success('Submitted successfully', { id: toastId });
+        setFirstName('');
+        setLastName('');
+        setEmail('');
+        setZip('');
+        setMobile('');
+        setTextarea('');
+        setTimeout(() =>  navigate('/'), 500);
+      }, 1500);
+    } else {
+      toast.error('You must be opted-in', { id: toastId });
+    }
   };
 
   return (
     <div className="page">
+      <Header
+        redText="CONTACT"
+        whiteText=""
+        children={[]}
+      />
+
       <form className={styles.wrapper} onSubmit={handleSubmit}>
         <label>
           First Name
-          <input type="text" />
+          <input
+            type="text"
+            value={firstName}
+            onChange={(e) => handleInputChange(e, 'firstName')}
+          />
         </label>
         <br />
 
         <label>
           Last Name
-          <input type="text" />
+          <input
+            type="text"
+            value={lastName}
+            onChange={(e) => handleInputChange(e, 'lastName')}
+          />
         </label>
         <br />
 
         <label>
           Email
-          <input type="text" />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => handleInputChange(e, 'email')}
+          />
         </label>
         <br />
 
         <label>
           ZIP
-          <input type="text" />
+          <input
+            type="text"
+            value={zip}
+            onChange={(e) => handleInputChange(e, 'textarea')}
+          />
         </label>
         <br />
 
         <label>
           Mobile
-          <input type="text" />
+          <input
+            type="text"
+            value={mobile}
+            onChange={(e) => handleInputChange(e, 'mobile')}
+          />
         </label>
         <br />
 
         <div className="checkbox-wrapper">
-          <input id="contact-checkbox" type="checkbox" />
+          <input
+            id="contact-checkbox"
+            type="checkbox"
+            checked={optIn}
+            onClick={() => setOptIn(!optIn)}
+            onChange={(e) => handleInputChange(e, 'optIn')}
+          />
           <label htmlFor="contact-checkbox">
             I would like to receive text messages from Elect Common Sense
           </label>
@@ -52,7 +141,10 @@ export default function Contact() {
 
         <label>
           Add your message
-          <textarea />
+          <textarea
+            value={textarea}
+            onChange={(e) => handleInputChange(e, 'textarea')}
+          />
         </label>
         <br />
 
