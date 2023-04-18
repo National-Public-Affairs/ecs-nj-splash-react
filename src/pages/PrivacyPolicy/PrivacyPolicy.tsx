eb1114afd67b25e8d30f596952160884baed1015
asb1512 <asb1512@gmail.com>
2023-04-18 12:33:20 -0400

    building out privacy policy

---
+++ src/pages/PrivacyPolicy/PrivacyPolicy.tsx
@@ -0,0 +1,17 @@
+import Header from '../../components/Header/Header';
+
+export default function PrivacyPolicy() {
+  return (
+    <div className="page">
+      <Header
+        redText=''
+        whiteText='PRIVACY POLICY'
+        children={[]}
+      />
+
+      <div className="text-content">
+
+      </div>
+    </div>
+  );
+}
